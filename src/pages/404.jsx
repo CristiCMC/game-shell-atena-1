d0d77f57741a81838990ab4d97392f8106874d9e
CristiCMC <100723697+CristiCMC@users.noreply.github.com>
2023-02-22 20:02:38 +0000

    curs 22.02.2023

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,17 +1,31 @@
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { FullscreenLayout } from '../layouts';
 
 export const FourOhFour = () => {
+  const dispatch = useDispatch();
+
   return (
     <FullscreenLayout>
-      <div class="bg-pink-400 w-screen h-screen flex items-center justify-center">
-        <div class="text-center text-white">
-          <span class="text-7xl animate-pulse block">404</span>
+      <div className="bg-pink-400 w-screen h-screen flex items-center justify-center">
+        <div className="text-center text-white">
+          <span className="text-7xl animate-pulse block">404</span>
 
           <Link to="/" title="Home" className="font-bold">
             Home
           </Link>
         </div>
+
+        <button
+          onClick={() => {
+            dispatch({
+              type: 'add',
+              payload: 404,
+            });
+          }}
+        >
+          Add 404 to state
+        </button>
       </div>
     </FullscreenLayout>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Layout } from '../layouts';
-import { client as userApi } from './../api/users';
+import { client as usersApi } from './../api/users';
 
 export const Home = () => {
   const [users, setUsers] = useState([]);
+  const dispatch = useDispatch();
+  const number = useSelector((state) => {
+    return state.counter;
+  });
 
   useEffect(() => {
-    userApi.get('/users').then((response) => {
+    usersApi.get('/users').then((response) => {
       const { data } = response;
 
       setUsers(data);
@@ -15,7 +20,31 @@ export const Home = () => {
 
   return (
     <Layout>
-      <div class="mx-auto px-4 container">Home: {JSON.stringify(users)}</div>
+      <div className="mx-auto px-4 container">
+        Home: {JSON.stringify(users)}
+        <p className="mt-6">{number}</p>
+        <div>
+          <button
+            onClick={() => {
+              dispatch({
+                type: 'increment',
+              });
+            }}
+          >
+            Increment +
+          </button>
+
+          <button
+            onClick={() => {
+              dispatch({
+                type: 'decrement',
+              });
+            }}
+          >
+            Decrement -{' '}
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 };
